55f7659f310a41143b1d1a1ab1481d509ed81009
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2021-01-11 23:13:35 -0300

    updated header

--- front-mobile/src/components/Header/index.tsx
+++ front-mobile/src/components/Header/index.tsx
@@ -1,12 +1,20 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 export default function Header() {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => navigation.navigate('Home');
+
   return (
-    <View style={styles.container}>
-        <Image source={require('../../assets/logo.png')}/>
-      <Text style={styles.text}>Ds Delivery</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.text}>Ds Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -18,17 +26,17 @@ const styles = StyleSheet.create({
     paddingBottom: 30,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor:'#da5c5c',
-    flexDirection:'row'
-    
+    backgroundColor: '#da5c5c',
+    flexDirection: 'row'
+
   },
-  text:{
-      color: '#fff',
-      fontSize: 20,
-      fontWeight: 'bold',
-      lineHeight: 25,
-      letterSpacing: -0.24,
-      marginLeft: 15,
-      fontFamily: 'OpenSans_700Bold',
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    lineHeight: 25,
+    letterSpacing: -0.24,
+    marginLeft: 15,
+    fontFamily: 'OpenSans_700Bold',
   }
 });
