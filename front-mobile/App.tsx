bc580b91c61639deacc7171b1d749918e9a9b5e9
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2021-01-11 23:15:21 -0300

    Created routes

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -3,8 +3,7 @@ import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Header from './src/components/Header';
-import Home from './src/components/Home';
+import Routes from './src/routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -19,8 +18,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Header/>
-      <Home/>
+      <Routes/>
     </View>
   );
 }
